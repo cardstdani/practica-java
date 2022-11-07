572a8f69a1cd319d4f6de5ef80874eddbda17ff3
Ivan Alvaro <ivanprogamer43@gmail.com>
2022-11-07 22:55:41 +0100

    COSITAS PA

--- Practica.java
+++ Practica.java
@@ -1,42 +1,33 @@
 import java.util.Scanner;
-import java.net.URL;
 import java.io.*;
 
 class Practica {
     public static void main(String[] args) {
+        String diccionario[] = generarDiccionario("./Diccionario.txt");
+        
+        
         System.out.println("Juguemos a Wordle");
         System.out.println("Piensa una palabra ...");
         System.out.println("Y dime si la acierto: ");
 
         // GENERAR PALABRA Y ESPERAR ENTRADA DE 0 Y 1 DEL USUARIO
-        //System.out.println(generateRandomWord());
-
-        try {
-            File doc = new File("./Diccionario.txt");
-            Scanner s = new Scanner(doc);
-
-            System.out.println(s.next());
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        System.out.println(generateRandomWord(diccionario));
     }
     
     // AQUI HABRIA QUE METERLE EN UN FUTURO ARGUMRNTOS PARA QUE GENERE DEPENDIENDO DE LA ENTRADA DEL USUARIO
-    public static String generateRandomWord() {
-        String word = "";
+    public static String generateRandomWord(String diccionario[]) {
+        return diccionario[(int)(Math.random() * diccionario.length + 1) - 1];
+    }
+
+    public static String[] generarDiccionario(String ruta){
+        String diccionario[] = {};
         try {
-            File doc = new File("./Diccionario.txt");
+            File doc = new File(ruta);
             Scanner s = new Scanner(doc);
-            int indexOfWord = (int) (Math.random() * 7893600 + 1);
-            for (int i = 0; i < indexOfWord-1; i++){
-                word = s.next();
-            }
-            //PUTO JAVA NO ME DEJA HACER AQUI EL RETURN
+            diccionario = (s.useDelimiter("\\A").next()).split(" ");
         } catch (IOException ex) {
             ex.printStackTrace();
-            word = "ERROR";
         }
-
-        return word.toUpperCase();
+        return diccionario;
     }
 }
