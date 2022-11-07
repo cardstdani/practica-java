901f97d1e380df3050a2f462811f562bde6fc67b
Daniel García Solla <72665778+cardstdani@users.noreply.github.com>
2022-11-07 13:52:00 +0100

    Update Practica.java

--- Practica.java
+++ Practica.java
@@ -3,14 +3,14 @@ import java.net.URL;
 import java.io.*;
 
 class Practica {
-  public static void main(String[] args) {
-    try {
-      URL url = new URL("https://raw.githubusercontent.com/cardstdani/practica-java/main/Diccionario.txt");
-      Scanner s = new Scanner(url.openStream());
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://raw.githubusercontent.com/cardstdani/practica-java/main/Diccionario.txt");
+            Scanner s = new Scanner(url.openStream());
 
-        System.out.println(s.next());
-    } catch (IOException ex) {
-      ex.printStackTrace();
+            System.out.println(s.next());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
-  }
 }
