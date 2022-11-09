2297eea35aa2d89ecad07fc41956e548be647980
Ivan44 <ivanprogamer43@gmail.com>
2022-11-09 12:29:44 +0100

    intentona

--- Practica.java
+++ Practica.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.io.*;
 
 class Practica {
+    final static int intentos = 5;
     public static void main(String[] args) {
         String diccionario[] = generarDiccionario("./Diccionario.txt");
         
@@ -10,12 +11,34 @@ class Practica {
         System.out.println("Piensa una palabra ...");
         System.out.println("Y dime si la acierto: ");
 
-        // GENERAR PALABRA Y ESPERAR ENTRADA DE 0 Y 1 DEL USUARIO
-        System.out.println(generateRandomWord(diccionario));
+        int entrada[] = {0,0,0,0,0}; 
+        String prevWord = "";
+        for(int intento = 0; intento < intentos; intento++){
+            String actualWord = generateRandomWord(diccionario, entrada, prevWord);
+            System.out.println(actualWord);
+            System.out.println("La hacerte ¿?: ");
+            Scanner in = new Scanner(System.in);
+            entrada = stringToIntArray(in.nextLine(), 5);
+            prevWord = actualWord;
+        }
     }
     
     // AQUI HABRIA QUE METERLE EN UN FUTURO ARGUMRNTOS PARA QUE GENERE DEPENDIENDO DE LA ENTRADA DEL USUARIO
-    public static String generateRandomWord(String diccionario[]) {
+    public static String generateRandomWord(String diccionario[], int entrada[], String prevWord) {
+        String result = "";
+        int prevIndex = 0;
+        String oldSelectedWord = "";
+        for(int i = 0; i < diccionario.length; i++){
+            for(int v = 0; v < entrada.length; v++){
+                if(entrada[v] == 1){
+                    
+                }else if(entrada[v] == 2){
+
+                }else{
+
+                }
+            }
+        }
         return diccionario[(int)(Math.random() * diccionario.length + 1) - 1];
     }
 
@@ -29,5 +52,14 @@ class Practica {
             ex.printStackTrace();
         }
         return diccionario;
+    }
+
+    public static int[] stringToIntArray(String in, int max){
+        int result[] = new int[max]; 
+        char[] descomposicion = in.toCharArray();
+        for (int i = 0; i < descomposicion.length && i < max; i++){
+            result[i] = Integer.parseInt(String.valueOf(descomposicion[i]));
+        }
+        return result;
     }
 }
