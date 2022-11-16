6dbea986870114888663fade5961a3e2393e5930
Daniel García Solla <72665778+cardstdani@users.noreply.github.com>
2022-11-16 15:25:02 +0100

    Añadida lógica de ganar y perder

--- PracticaPRO.java
+++ PracticaPRO.java
@@ -24,74 +24,82 @@ class PracticaPRO {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String[] diccionario = generarDiccionario(".\\Diccionario2.txt");
-        String[] diccionarioOriginal = diccionario;
 
-        System.out.println("Juguemos a Wordle");
-        System.out.println("Piensa una palabra ...");
-        System.out.println("Y dime si la acierto: ");
+        while (true) {
+            String[] diccionario = generarDiccionario(".\\Diccionario2.txt");
+            String[] diccionarioOriginal = diccionario;
 
-        int[] entrada;
+            System.out.println("Juguemos a Wordle");
+            System.out.println("Piensa una palabra ...");
+            System.out.println("Y dime si la acierto: ");
 
-        /**
-         * En este array se almacenan las letras que pueden ir en cada posicion en el primer sub-array. En el segundo, todas las que deben ir
-         */
-        ArrayList<Letra> posibleEstructura = new ArrayList<>(maxInputLength);
-        IntStream.range(0, maxInputLength).forEach(i -> posibleEstructura.add(new Letra(abecedario, new ArrayList<>())));
-        for (int intento = 0; intento < intentos; intento++) {
-            System.out.println(diccionario.length);
-            String word = diccionario.length > 0 ? diccionario[r.nextInt(diccionario.length)] : diccionarioOriginal[r.nextInt(diccionarioOriginal.length)];
-            System.out.println(word);
-            entrada = stringToIntArray(in.nextLine());
+            int[] entrada = {0, 0, 0, 0, 0};
 
-            if (validar(entrada, word, posibleEstructura)) {
-                for (int i = 0; i < entrada.length; i++) {
-                    char letra = word.charAt(i);
-                    switch (entrada[i]) {
-                        case 0: {
-                            for (int a = 0; a < posibleEstructura.size(); a++) {
-                                if (posibleEstructura.get(a).estan.size() != 1 & posibleEstructura.get(a).estan.contains(letra))
-                                    posibleEstructura.get(a).estan.remove(letra);
-                            }
-                            break;
-                        }
-                        case 1: {
-                            for (int a = 0; a < posibleEstructura.size(); a++) {
-                                if (a == i) {
-                                    posibleEstructura.get(a).estan.remove(letra);
-                                } else {
-                                    if (posibleEstructura.get(a).estan.size() != 1)
-                                        posibleEstructura.get(a).debenEstar.add(letra);
+            /**
+             * En este array se almacenan las letras que pueden ir en cada posicion en el primer sub-array. En el segundo, todas las que deben ir
+             */
+            ArrayList<Letra> posibleEstructura = new ArrayList<>(maxInputLength);
+            IntStream.range(0, maxInputLength).forEach(i -> posibleEstructura.add(new Letra(abecedario, new ArrayList<>())));
+            for (int intento = 0; (intento < intentos) & !Arrays.equals(entrada, new int[]{2, 2, 2, 2, 2}); intento++) {
+                System.out.println(diccionario.length);
+                String word = diccionario.length > 0 ? diccionario[r.nextInt(diccionario.length)] : diccionarioOriginal[r.nextInt(diccionarioOriginal.length)];
+                System.out.println(word);
+                entrada = stringToIntArray(in.next());
+
+                if (validar(entrada, word, posibleEstructura)) {
+                    for (int i = 0; i < entrada.length; i++) {
+                        char letra = word.charAt(i);
+                        switch (entrada[i]) {
+                            case 0: {
+                                for (Letra a : posibleEstructura) {
+                                    if (a.estan.size() != 1 & a.estan.contains(letra))
+                                        a.estan.remove(letra);
                                 }
+                                break;
                             }
-                            break;
-                        }
-                        case 2: {
-                            for (int a = 0; a < posibleEstructura.size(); a++) {
-                                if (a == i) {
-                                    posibleEstructura.set(i, new Letra(new HashSet<>(Arrays.asList(letra)), new ArrayList<>()));
-                                } else {
-                                    if (posibleEstructura.get(a).debenEstar.contains(letra))
-                                        posibleEstructura.get(a).debenEstar.remove((Character) letra);
+                            case 1: {
+                                for (int a = 0; a < posibleEstructura.size(); a++) {
+                                    if (a == i) {
+                                        posibleEstructura.get(a).estan.remove(letra);
+                                    } else {
+                                        if (posibleEstructura.get(a).estan.size() != 1)
+                                            posibleEstructura.get(a).debenEstar.add(letra);
+                                    }
                                 }
+                                break;
                             }
-                            break;
+                            case 2: {
+                                for (int a = 0; a < posibleEstructura.size(); a++) {
+                                    if (a == i) {
+                                        posibleEstructura.set(i, new Letra(new HashSet<>(Arrays.asList(letra)), new ArrayList<>()));
+                                    } else {
+                                        if (posibleEstructura.get(a).debenEstar.contains(letra))
+                                            posibleEstructura.get(a).debenEstar.remove((Character) letra);
+                                    }
+                                }
+                                break;
+                            }
                         }
+                    }
+
+                    diccionario = updateDict(diccionario, posibleEstructura);
+                } else {
+                    if (Arrays.equals(entrada, new int[]{2, 2, 2, 2, 2})) {
+                        System.out.println("Error, aunque se considera ganador");
+                        break;
+                    } else {
+                        System.out.println("Error, intenta otra vez");
+                        intento--;
                     }
                 }
 
-                diccionario = updateDict(diccionario, posibleEstructura);
-            } else {
-                if (entrada == new int[]{2, 2, 2, 2, 2}) {
-                    System.out.println("Error, aunque se considera ganador");
-                    break;
-                } else {
-                    System.out.println("Error, intenta otra vez");
-                    intento--;
-                }
+                System.out.println(Arrays.deepToString(posibleEstructura.toArray()));
             }
 
-            System.out.println(Arrays.deepToString(posibleEstructura.toArray()));
+            boolean result = Arrays.equals(entrada, new int[]{2, 2, 2, 2, 2});
+            System.out.println(result ? "GANAR!!" : "PERDER!!");
+            System.out.println("Quiere usted jugar una partida más?");
+            if(in.next().equalsIgnoreCase("no")) break;
         }
     }
 
@@ -120,7 +128,7 @@ class PracticaPRO {
                         }
                     }
 
-                    if ((posibleEstructura.get(i).estan.size() == 1 & posibleEstructura.get(i).estan.contains(letra)) | posibleEstructura.get(i).debenEstar.contains(letra)) {
+                    if (posibleEstructura.get(i).estan.size() == 1 & posibleEstructura.get(i).estan.contains(letra)) {
                         return false;
                     }
                     break;
@@ -131,8 +139,8 @@ class PracticaPRO {
                     }
 
                     boolean result = true;
-                    for (int a = 0; a < posibleEstructura.size(); a++) {
-                        if (posibleEstructura.get(a).estan.contains(letra)) {
+                    for (Letra a : posibleEstructura) {
+                        if (a.estan.contains(letra)) {
                             result = false;
                             break;
                         }
@@ -151,22 +159,22 @@ class PracticaPRO {
 
     public static String[] updateDict(String[] diccionario, ArrayList<Letra> estan) {
         ArrayList<String> tmpDict = new ArrayList<>();
-        for (int i = 0; i < diccionario.length; i++) {
+        for (String i : diccionario) {
             boolean result = true;
-            for (int j = 0; j < diccionario[i].length(); j++) {
-                char letra = diccionario[i].charAt(j);
+            for (int j = 0; j < i.length(); j++) {
+                char letra = i.charAt(j);
                 if (!estan.get(j).estan.contains(letra) & !estan.get(j).debenEstar.contains(letra)) {
-                    result &= false;
+                    result = false;
                     break;
                 }
             }
-            if (result) tmpDict.add(diccionario[i]);
+            if (result) tmpDict.add(i);
         }
         return tmpDict.toArray(new String[0]);
     }
 
     public static String[] generarDiccionario(String ruta) {
-        String diccionario[] = {};
+        String[] diccionario = {};
         try {
             File doc = new File(ruta);
             Scanner s = new Scanner(doc);
@@ -179,7 +187,7 @@ class PracticaPRO {
 
     public static int[] stringToIntArray(String in) {
         if (in.length() < 5) return new int[]{-1};
-        int result[] = new int[maxInputLength];
+        int[] result = new int[maxInputLength];
         ArrayList<Character> descomposicion = new ArrayList<>(in.chars().mapToObj(i -> (char) i).collect(Collectors.toList()));
 
         for (int i = 0; i < descomposicion.size() && i < maxInputLength; i++) {
