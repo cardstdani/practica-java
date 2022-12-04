9c5cb5036b01af71b415aa525842405732736e9c
Daniel García Solla <72665778+cardstdani@users.noreply.github.com>
2022-12-04 09:20:40 +0100

    Versión diccionarios cargados desde URL

--- PracticaRegex.java
+++ PracticaRegex.java
@@ -5,11 +5,6 @@ import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.internal.LinkedTreeMap;
-
 class ParalelMapUpdate implements Callable<LinkedHashMap<String, Double>> {
     int thread, chunkSize;
     String[] dict;
@@ -17,7 +12,7 @@ class ParalelMapUpdate implements Callable<LinkedHashMap<String, Double>> {
     public static double scoreWord(String word, String[] dict) {
         double finalScore = 0;
 
-        for (String i : PracticaRegex.combinations) { //Por cada elemento en combinaciones
+        for (int[] i : PracticaRegex.combinations) { //Por cada elemento en combinaciones
             String pattern = PracticaRegex.generatePattern(i, word);
 
             double p = IntStream.range(0, dict.length).mapToDouble(j -> (dict[j].matches(pattern) ? 1.0 : 0.0)).parallel().sum();
@@ -45,23 +40,15 @@ class ParalelMapUpdate implements Callable<LinkedHashMap<String, Double>> {
 }
 
 class PracticaRegex {
-    public final static String[] combinations = {"00000", "00001", "00002", "00010", "00011", "00012", "00020", "00021", "00022", "00100", "00101", "00102", "00110", "00111", "00112", "00120", "00121", "00122", "00200", "00201", "00202", "00210", "00211", "00212", "00220", "00221", "00222", "01000", "01001", "01002", "01010", "01011", "01012", "01020", "01021", "01022", "01100", "01101", "01102", "01110", "01111", "01112", "01120", "01121", "01122", "01200", "01201", "01202", "01210", "01211", "01212", "01220", "01221", "01222", "02000", "02001", "02002", "02010", "02011", "02012", "02020", "02021", "02022", "02100", "02101", "02102", "02110", "02111", "02112", "02120", "02121", "02122", "02200", "02201", "02202", "02210", "02211", "02212", "02220", "02221", "02222", "10000", "10001", "10002", "10010", "10011", "10012", "10020", "10021", "10022", "10100", "10101", "10102", "10110", "10111", "10112", "10120", "10121", "10122", "10200", "10201", "10202", "10210", "10211", "10212", "10220", "10221", "10222", "11000", "11001", "11002", "11010", "11011", "11012", "11020", "11021", "11022", "11100", "11101", "11102", "11110", "11111", "11112", "11120", "11121", "11122", "11200", "11201", "11202", "11210", "11211", "11212", "11220", "11221", "11222", "12000", "12001", "12002", "12010", "12011", "12012", "12020", "12021", "12022", "12100", "12101", "12102", "12110", "12111", "12112", "12120", "12121", "12122", "12200", "12201", "12202", "12210", "12211", "12212", "12220", "12221", "12222", "20000", "20001", "20002", "20010", "20011", "20012", "20020", "20021", "20022", "20100", "20101", "20102", "20110", "20111", "20112", "20120", "20121", "20122", "20200", "20201", "20202", "20210", "20211", "20212", "20220", "20221", "20222", "21000", "21001", "21002", "21010", "21011", "21012", "21020", "21021", "21022", "21100", "21101", "21102", "21110", "21111", "21112", "21120", "21121", "21122", "21200", "21201", "21202", "21210", "21211", "21212", "21220", "21221", "21222", "22000", "22001", "22002", "22010", "22011", "22012", "22020", "22021", "22022", "22100", "22101", "22102", "22110", "22111", "22112", "22120", "22121", "22122", "22200", "22201", "22202", "22210", "22211", "22212", "22220", "22221", "22222"};
+    public final static int[][] combinations = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 1}, {0, 0, 0, 0, 2}, {0, 0, 0, 1, 0}, {0, 0, 0, 1, 1}, {0, 0, 0, 1, 2}, {0, 0, 0, 2, 0}, {0, 0, 0, 2, 1}, {0, 0, 0, 2, 2}, {0, 0, 1, 0, 0}, {0, 0, 1, 0, 1}, {0, 0, 1, 0, 2}, {0, 0, 1, 1, 0}, {0, 0, 1, 1, 1}, {0, 0, 1, 1, 2}, {0, 0, 1, 2, 0}, {0, 0, 1, 2, 1}, {0, 0, 1, 2, 2}, {0, 0, 2, 0, 0}, {0, 0, 2, 0, 1}, {0, 0, 2, 0, 2}, {0, 0, 2, 1, 0}, {0, 0, 2, 1, 1}, {0, 0, 2, 1, 2}, {0, 0, 2, 2, 0}, {0, 0, 2, 2, 1}, {0, 0, 2, 2, 2}, {0, 1, 0, 0, 0}, {0, 1, 0, 0, 1}, {0, 1, 0, 0, 2}, {0, 1, 0, 1, 0}, {0, 1, 0, 1, 1}, {0, 1, 0, 1, 2}, {0, 1, 0, 2, 0}, {0, 1, 0, 2, 1}, {0, 1, 0, 2, 2}, {0, 1, 1, 0, 0}, {0, 1, 1, 0, 1}, {0, 1, 1, 0, 2}, {0, 1, 1, 1, 0}, {0, 1, 1, 1, 1}, {0, 1, 1, 1, 2}, {0, 1, 1, 2, 0}, {0, 1, 1, 2, 1}, {0, 1, 1, 2, 2}, {0, 1, 2, 0, 0}, {0, 1, 2, 0, 1}, {0, 1, 2, 0, 2}, {0, 1, 2, 1, 0}, {0, 1, 2, 1, 1}, {0, 1, 2, 1, 2}, {0, 1, 2, 2, 0}, {0, 1, 2, 2, 1}, {0, 1, 2, 2, 2}, {0, 2, 0, 0, 0}, {0, 2, 0, 0, 1}, {0, 2, 0, 0, 2}, {0, 2, 0, 1, 0}, {0, 2, 0, 1, 1}, {0, 2, 0, 1, 2}, {0, 2, 0, 2, 0}, {0, 2, 0, 2, 1}, {0, 2, 0, 2, 2}, {0, 2, 1, 0, 0}, {0, 2, 1, 0, 1}, {0, 2, 1, 0, 2}, {0, 2, 1, 1, 0}, {0, 2, 1, 1, 1}, {0, 2, 1, 1, 2}, {0, 2, 1, 2, 0}, {0, 2, 1, 2, 1}, {0, 2, 1, 2, 2}, {0, 2, 2, 0, 0}, {0, 2, 2, 0, 1}, {0, 2, 2, 0, 2}, {0, 2, 2, 1, 0}, {0, 2, 2, 1, 1}, {0, 2, 2, 1, 2}, {0, 2, 2, 2, 0}, {0, 2, 2, 2, 1}, {0, 2, 2, 2, 2}, {1, 0, 0, 0, 0}, {1, 0, 0, 0, 1}, {1, 0, 0, 0, 2}, {1, 0, 0, 1, 0}, {1, 0, 0, 1, 1}, {1, 0, 0, 1, 2}, {1, 0, 0, 2, 0}, {1, 0, 0, 2, 1}, {1, 0, 0, 2, 2}, {1, 0, 1, 0, 0}, {1, 0, 1, 0, 1}, {1, 0, 1, 0, 2}, {1, 0, 1, 1, 0}, {1, 0, 1, 1, 1}, {1, 0, 1, 1, 2}, {1, 0, 1, 2, 0}, {1, 0, 1, 2, 1}, {1, 0, 1, 2, 2}, {1, 0, 2, 0, 0}, {1, 0, 2, 0, 1}, {1, 0, 2, 0, 2}, {1, 0, 2, 1, 0}, {1, 0, 2, 1, 1}, {1, 0, 2, 1, 2}, {1, 0, 2, 2, 0}, {1, 0, 2, 2, 1}, {1, 0, 2, 2, 2}, {1, 1, 0, 0, 0}, {1, 1, 0, 0, 1}, {1, 1, 0, 0, 2}, {1, 1, 0, 1, 0}, {1, 1, 0, 1, 1}, {1, 1, 0, 1, 2}, {1, 1, 0, 2, 0}, {1, 1, 0, 2, 1}, {1, 1, 0, 2, 2}, {1, 1, 1, 0, 0}, {1, 1, 1, 0, 1}, {1, 1, 1, 0, 2}, {1, 1, 1, 1, 0}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 2}, {1, 1, 1, 2, 0}, {1, 1, 1, 2, 1}, {1, 1, 1, 2, 2}, {1, 1, 2, 0, 0}, {1, 1, 2, 0, 1}, {1, 1, 2, 0, 2}, {1, 1, 2, 1, 0}, {1, 1, 2, 1, 1}, {1, 1, 2, 1, 2}, {1, 1, 2, 2, 0}, {1, 1, 2, 2, 1}, {1, 1, 2, 2, 2}, {1, 2, 0, 0, 0}, {1, 2, 0, 0, 1}, {1, 2, 0, 0, 2}, {1, 2, 0, 1, 0}, {1, 2, 0, 1, 1}, {1, 2, 0, 1, 2}, {1, 2, 0, 2, 0}, {1, 2, 0, 2, 1}, {1, 2, 0, 2, 2}, {1, 2, 1, 0, 0}, {1, 2, 1, 0, 1}, {1, 2, 1, 0, 2}, {1, 2, 1, 1, 0}, {1, 2, 1, 1, 1}, {1, 2, 1, 1, 2}, {1, 2, 1, 2, 0}, {1, 2, 1, 2, 1}, {1, 2, 1, 2, 2}, {1, 2, 2, 0, 0}, {1, 2, 2, 0, 1}, {1, 2, 2, 0, 2}, {1, 2, 2, 1, 0}, {1, 2, 2, 1, 1}, {1, 2, 2, 1, 2}, {1, 2, 2, 2, 0}, {1, 2, 2, 2, 1}, {1, 2, 2, 2, 2}, {2, 0, 0, 0, 0}, {2, 0, 0, 0, 1}, {2, 0, 0, 0, 2}, {2, 0, 0, 1, 0}, {2, 0, 0, 1, 1}, {2, 0, 0, 1, 2}, {2, 0, 0, 2, 0}, {2, 0, 0, 2, 1}, {2, 0, 0, 2, 2}, {2, 0, 1, 0, 0}, {2, 0, 1, 0, 1}, {2, 0, 1, 0, 2}, {2, 0, 1, 1, 0}, {2, 0, 1, 1, 1}, {2, 0, 1, 1, 2}, {2, 0, 1, 2, 0}, {2, 0, 1, 2, 1}, {2, 0, 1, 2, 2}, {2, 0, 2, 0, 0}, {2, 0, 2, 0, 1}, {2, 0, 2, 0, 2}, {2, 0, 2, 1, 0}, {2, 0, 2, 1, 1}, {2, 0, 2, 1, 2}, {2, 0, 2, 2, 0}, {2, 0, 2, 2, 1}, {2, 0, 2, 2, 2}, {2, 1, 0, 0, 0}, {2, 1, 0, 0, 1}, {2, 1, 0, 0, 2}, {2, 1, 0, 1, 0}, {2, 1, 0, 1, 1}, {2, 1, 0, 1, 2}, {2, 1, 0, 2, 0}, {2, 1, 0, 2, 1}, {2, 1, 0, 2, 2}, {2, 1, 1, 0, 0}, {2, 1, 1, 0, 1}, {2, 1, 1, 0, 2}, {2, 1, 1, 1, 0}, {2, 1, 1, 1, 1}, {2, 1, 1, 1, 2}, {2, 1, 1, 2, 0}, {2, 1, 1, 2, 1}, {2, 1, 1, 2, 2}, {2, 1, 2, 0, 0}, {2, 1, 2, 0, 1}, {2, 1, 2, 0, 2}, {2, 1, 2, 1, 0}, {2, 1, 2, 1, 1}, {2, 1, 2, 1, 2}, {2, 1, 2, 2, 0}, {2, 1, 2, 2, 1}, {2, 1, 2, 2, 2}, {2, 2, 0, 0, 0}, {2, 2, 0, 0, 1}, {2, 2, 0, 0, 2}, {2, 2, 0, 1, 0}, {2, 2, 0, 1, 1}, {2, 2, 0, 1, 2}, {2, 2, 0, 2, 0}, {2, 2, 0, 2, 1}, {2, 2, 0, 2, 2}, {2, 2, 1, 0, 0}, {2, 2, 1, 0, 1}, {2, 2, 1, 0, 2}, {2, 2, 1, 1, 0}, {2, 2, 1, 1, 1}, {2, 2, 1, 1, 2}, {2, 2, 1, 2, 0}, {2, 2, 1, 2, 1}, {2, 2, 1, 2, 2}, {2, 2, 2, 0, 0}, {2, 2, 2, 0, 1}, {2, 2, 2, 0, 2}, {2, 2, 2, 1, 0}, {2, 2, 2, 1, 1}, {2, 2, 2, 1, 2}, {2, 2, 2, 2, 0}, {2, 2, 2, 2, 1}, {2, 2, 2, 2, 2}};
     public final static int intentos = 6, maxInputLength = 5;
     public final static int n = Runtime.getRuntime().availableProcessors(); // numero de hilos
-    public static LinkedTreeMap<String, LinkedTreeMap<String, String>> map = new LinkedTreeMap<>();
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        try {
-            JsonElement json = JsonParser.parseReader(new InputStreamReader(new URL("https://github.com/cardstdani/practica-java/blob/main/Data/patterns_data.txt?raw=true").openStream(), "UTF-8"));
-            Gson gson = new Gson();
-            map = (LinkedTreeMap<String, LinkedTreeMap<String, String>>) gson.fromJson(json, map.getClass());
-        } catch (Exception e) {
-            System.out.println("Error cargando patrones");
-        }
 
         do {
-            LinkedHashMap<String, Double> diccionario = generarDiccionario("https://raw.githubusercontent.com/cardstdani/practica-java/main/Data/Diccionario3.txt");
+            LinkedHashMap<String, Double> diccionario = generarDiccionario("https://raw.githubusercontent.com/cardstdani/practica-java/main/Data/Diccionario4.txt");
             LinkedHashMap<String, Double> diccionarioOriginal = diccionario;
 
             System.out.println("Juguemos a Wordle\nPiensa una palabra ...\nY dime si la acierto: ");
@@ -76,8 +63,8 @@ class PracticaRegex {
                 System.out.println(word);
                 entrada = stringToIntArray(in.next());
 
+                String pattern = generatePattern(entrada, word);
                 if (validar(entrada, word, globalPattern)) {
-                    String pattern = generatePattern(Arrays.stream(entrada).mapToObj(String::valueOf).collect(Collectors.joining("")), word);
                     globalPattern += pattern.substring(1, pattern.length() - 3);
                     diccionario = updateDict(diccionario, pattern);
                 } else {
@@ -98,8 +85,25 @@ class PracticaRegex {
         System.exit(0);
     }
 
-    public static String generatePattern(String entrada, String word) {
-        return map.get(word).get(entrada);
+    public static String generatePattern(int[] entrada, String word) {
+        StringBuilder pattern = new StringBuilder();
+        LinkedHashSet<Character> procesed = new LinkedHashSet<>();
+
+        for (int j = 0; j < entrada.length; j++) {
+            char letra = word.charAt(j);
+            if (!procesed.contains(letra)) {
+                boolean condition = IntStream.range(j + 1, entrada.length).mapToLong(k -> (word.charAt(k) == letra & entrada[k] == 2 ? 1 : 0)).sum() > 0;
+                if (entrada[j] == 0) {
+                    if (!condition) procesed.add(letra);
+                } else {
+                    procesed.add(letra);
+                }
+                pattern.append(new String[]{!condition ? String.format("(?=[^%s]*$)", letra) : String.format("(?!.{%s}%s)", j, letra), String.format("(?!.{%s}%s)(?=.*%s)", j, letra, letra),
+                        String.format("(?=.{%s}%s)", j, letra)}[entrada[j]]);
+            }
+        }
+
+        return String.format("^%s.*$", pattern);
     }
 
     public static boolean validar(int[] entrada, String word, String globalPattern) {
@@ -150,8 +154,7 @@ class PracticaRegex {
             try {
                 tmpDict.putAll(t.get());
             } catch (Exception e) {
-                System.out.println("Error en threads");
-                e.printStackTrace();
+                System.out.println(e.getStackTrace().toString());
             }
         }
         return tmpDict;
